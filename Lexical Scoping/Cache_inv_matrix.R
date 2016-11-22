69b717675d7a14c70888a417c415ebc51857fe98
Rod <rod.paris@hotmail.fr>
2016-11-22 22:55:46 +0100

    Add files via upload

---
+++ Lexical Scoping/Cache_inv_matrix.R
@@ -0,0 +1,46 @@
+# Rod Paris
+# Lexical Scoping
+# 2016
+
+# Matrix inversion is usually a costly computation and there may be some benefit
+# to caching the inverse of a matrix rather than compute it repeatedly. 
+# Below are two functions that are used to create a special object that 
+# stores a matrix and cache's its inverse.
+
+# makeCacheMatrix creates a list containing a function to
+# 1. set the value of the matrix
+# 2. get the value of the matrix
+# 3. set the value of inverse of the matrix
+# 4. get the value of inverse of the matrix
+
+makeCacheMatrix <- function(x = matrix()) {
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(inverse) inv <<- inverse
+        getInverse <- function() inv
+        list(set = set,
+             get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
+}
+
+# cacheSolve returns the inverse of the matrix (created by makeCacheMatrix) 
+# and stores it in the cache via setInverse. 
+# If the inverse has already been calculated (and the matrix has not changed),
+# the cacheSolve retrieves the inverse from the cache.
+
+cacheSolve <- function(x, ...) {
+        inv <- x$getInverse()
+        if(!is.null(inv)) {
+                message("getting cached data.")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setInverse(inv)
+        inv
+}
